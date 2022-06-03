fbfd0e2e513b2e4ecad8906dea55989b991e3cea
somanshu63 <somanshu63@gmail.com>
2022-06-03 14:19:56 +0530

    learn how to make disco grid

---
+++ block-BJaael/code/script.js
@@ -0,0 +1,34 @@
+let allbox = document.querySelector(".allboxes");
+allbox.classList.add("flex");
+let div = document.createElement("div");
+let boxcolor = document.querySelector(".box");
+
+for (let i = 0; i < 250; i++) {
+div[i] = document.createElement("div");
+div[i].classList.add("box");
+allbox.append(div[i]);
+let boxcolor = document.querySelector(".box");
+
+boxcolor[i] = document.children[0].children[1].children[1].children[i];
+
+allbox.addEventListener("mousemove", function() {
+    boxcolor[i].style.backgroundColor = random();
+    function random() {
+        rc = "#";
+        for(let i = 0; i < 6; i++){
+            rc += Math.floor(Math.random()*16).toString(16);
+        }
+        return rc;
+    }
+    boxcolor[i].innerText = randomText();
+    function randomText () {
+        rt = "";
+        for(let i = 0; i < 3; i++){
+            rt += Math.floor(Math.random()*10).toString(10);
+        }
+        return rt;
+    }
+});
+}
+
+
